bc900a0bf9c1d5cb8015967a0597b5b828e20704
wuzirui <wuzirui@tztyun.org>
2022-08-16 18:43:39 +0800

    fix bug

--- datasets/ray_utils.py
+++ datasets/ray_utils.py
@@ -19,7 +19,7 @@ def get_ray_directions(H, W, focal):
     # the direction here is without +0.5 pixel centering as calibration is not so accurate
     # see https://github.com/bmild/nerf/issues/24
     directions = \
-        torch.stack([(i-W/2)/focal, -(j-H/2)/focal, -torch.ones_like(i)], -1) # (H, W, 3)
+        torch.stack([(i + 0.5 - W / 2)/focal, -(j + 0.5 - H / 2)/focal, -torch.ones_like(i)], -1) # (H, W, 3)
 
     return directions
 
